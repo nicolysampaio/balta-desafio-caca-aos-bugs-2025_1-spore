cbaad164a3921b662d2e9352d45c5f114137089e
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-10-29 19:03:07 -0300

    feat: add initial project structure with database context and migrations

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cnnString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services
+  .AddDbContext<AppDbContext>(dbBuilder => dbBuilder.UseSqlite(cnnString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
